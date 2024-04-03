bb160b846ed6857ccd157e4a0969ea83cd6dfca5
Aqsa Shaikh <160129099+Aqsamajeed00@users.noreply.github.com>
2024-04-03 18:45:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+"use strict";
+// let message = "Hello World";
+// message = "Aqsa"
+// console.log(message)
+// const by = "my name is Aqsa"
+// console.log(by)
+// let fullName = "Aqsa Shaikh";
+// console.log("Hello");
+// booleon statement of true and false
+// booleon
+// let firstName = "Aqsa";
+// let age = 18;
+// let isMarried = false;
+// let num1 ="11"
+// let num2 ="11"
+// console.log(num1+num2);
+// let num1 ="14"
+// let num2 ="14"
+// console.log(num1 / num2);
+// let num1 = "11"
+// let num2 = "11"
+// console.log(num1 * num2);
+// let num1 = "88"
+// let num2 = "99"
+// console.log(num1 \ num2);
+// let num1 = "88"
+// let num2 = "99"
+// console.log(num1 / num2);
+// let X2 = "Aqsa Shaikh"
+// console.log(X2);
+// let X2 = "Variable in Typescript"
+// console.log(X2);
+// console.log(X2);
+// console.log(X2);
+// console.log(X2);
+// numrber datatypes
+// string datatypes
+// boolean datatypes
+// let box:number = 90;
+// console.log(box);
+// let myString:string = "Hey Everyone It's Mee!!"
+// console.log(myString);
+// const myBool: boolean =true
+// console.log(myBool);
+// Multi Line String
+// let multi = `Hello`+
+// ` I am Aqsa Shaikh`
+// console.log(multi);
+// let multi = `Hello\n`+
+// ` I am Aqsa Shaikh`
+// console.log(multi);
+// //Template Literals:
+// let fname:string="Miraal"
+// let sname:string="Shaikh"
+//  //console.log(fname + " " + sname);
+// //console.log(`${"fname"}`);
+// console.log(`${"sname"}`);
+// //PRACTICE!!!!
+// let message = "Hello Typescript"
+//  message = "Hello Typescript"
+//  console.log (message);
+// let boolean = "False"
+// console.log (boolean)
+// let number = 12
+// let A =
+// console.log (A);
+// let X3 = "Aqsa Shaikh"
+// console.log(X3);
+let statement = "LockManager";
+statement = "Aqsa Shaikh";
+console.log(statement);
+let B = 22;
+let C = 24;
+console.log(B + C);
+let boolean = ("is i am marriad");
+boolean = "True False";
+console.log("False");
+let car = ["Honda", "Civic", "Corola"];
+let car1 = "Honda";
+console.log(car1);
+let juice = ["Mango", "Peech", "Apple", "Watermelon"];
+let juice1 = ["All juices are very good"];
+console.log(juice1);
+let princess = ["Cindrella", "Fairytall", "Barbie"];
+let princess2 = "C";
+console.log("princess");
+let countdown = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(countdown);
